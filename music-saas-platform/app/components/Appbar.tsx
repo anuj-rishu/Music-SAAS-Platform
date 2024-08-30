e3d3dbf6d32dc68fc3318d419e8530f087e792d9
anujrisjhu <at6272@srmist.edu.in>
2024-08-30 23:55:20 +0530

    added schema

--- music-saas-platform/app/components/Appbar.tsx
+++ music-saas-platform/app/components/Appbar.tsx
@@ -2,16 +2,20 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Appbar() {
-    const  session = useSession();
-    return (
-        <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
-            <div>
-                <h1 className="text-2xl">Music Saas Platform</h1>
-            </div>
-            <div>
-                { session.data?.user && <button onClick={()=> signOut()} > Logout</button>}
-                { !session.data?.user &&  <button onClick={() => signIn()}>Sign In</button>}
-            </div>
-        </div>
-    );
+  const session = useSession();
+  return (
+    <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div>
+        <h1 className="text-2xl">Music Saas Platform</h1>
+      </div>
+      <div>
+        {session.data?.user && (
+          <button onClick={() => signOut()}> Logout</button>
+        )}
+        {!session.data?.user && (
+          <button onClick={() => signIn()}>Sign In</button>
+        )}
+      </div>
+    </div>
+  );
 }
---
+++ music-saas-platform/app/lib/db.ts
@@ -0,0 +1,5 @@
+import { PrismaClient as Prisma } from "@prisma/client";
+
+export const prismaClient : Prisma = new Prisma();
+
+
