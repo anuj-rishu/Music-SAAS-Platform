4ed2d3454738fa652874311519687db51c2ff817
anujrisjhu <at6272@srmist.edu.in>
2024-09-09 01:40:48 +0530

    added postgress

--- music-saas-platform/app/api/streams/route.ts
+++ music-saas-platform/app/api/streams/route.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-//@ts-ignore
+
 import youtubesearchapi from "youtube-search-api";
 import { YT_REGEX } from "@/app/lib/utils";
 import { getServerSession } from "next-auth";
